a14eb5a5fc3735f97a5a56d4983200d9e82f8efe
fenwick-t <fenwick-t@users.noreply.github.com>
2015-05-15 09:45:25 -0700

    CreateFile.ps1
    Script to create the file in GitHub repo

---
+++ CreateFile.ps1
@@ -0,0 +1,33 @@
+# Assume we have:
+# 1. Valid token
+# 2. Filename we want to create and content of the new file
+# 3. Username, repo name and branch name
+# This script will create a new file with given content into user's branch
+
+$username = "fenwick-t";
+$repo = "MyRepo1";
+
+# headers parametres
+$token = "1de89fc96275fe1f76683502a1830fe881ea504f";
+$headers = @{"Authorization" = "token $token"};
+
+# body parametres
+# path to the file (required)
+$path = "myTestFile.txt";
+
+# comment to the commit (required)
+$message = "create file (source control)";
+
+# content of the file (required, base64 encoded)
+#$base64Content = "bXkgbmV3IGZpbGUgY29udGVudHM=";
+$content = "I need to be encoded";
+$bytes = [System.Text.Encoding]::UTF8.GetBytes($content);
+$base64Content = [System.Convert]::ToBase64String($bytes);
+
+# branch name (optional) - "master" by default
+$branch = "master";
+
+$parametres = @{"path" = $path; "message" = $message; "content" = $base64Content; "branch" = $branch};
+$jsonBody = ConvertTo-Json $parametres;
+
+invoke-WebRequest -Method Put -Uri "https://api.github.com/repos/$username/$repo/contents/$path" -Body $jsonBody -Headers $headers
