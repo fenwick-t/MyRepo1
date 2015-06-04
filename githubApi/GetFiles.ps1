ad321e44e3eafd65ea2a5051c6550264fb3c3514
fenwick-t <fenwick-t@users.noreply.github.com>
2015-06-04 08:24:09 -0700

    Update GetFiles.ps1

--- githubApi/GetFiles.ps1
+++ githubApi/GetFiles.ps1
@@ -36,15 +36,74 @@ workflow Sync-GitHubRunbooks
     # headers parameters
     $headers = @{"Authorization" = "token $OAuthToken"};
     
-    # get username by token
+    # get username
     $jsonMetadata = Invoke-WebRequest -Method Get -Uri "https://api.github.com/user" -Headers $headers;
     $metadata = ConvertFrom-Json $jsonMetadata;
     $username = $metadata.login;
 
-    $Uri = "https://api.github.com/repos/$username/$repo/git/refs/heads/$branch";
+    # get branch sha
+    $Uri = "https://api.github.com/repos/$username/$Repo/git/refs/heads/$Branch";
     $jsonResults = invoke-WebRequest -Method Get -Uri $Uri -Headers $headers;
     $results = ConvertFrom-Json $jsonResults;
-    $results
+    $branchSha = $results.object.sha;
+
+    #get sha of the last commit from this branch
+    $Uri = "https://api.github.com/repos/$username/$Repo/commits?sha=$branchSha";
+    $jsonResults = invoke-WebRequest -Method Get -Uri $Uri -Headers $headers;
+    $commits = ConvertFrom-Json $jsonResults;    
+    $commitSha = $commits[0].sha;
+
+    # get sha of branch's tree
+    $Uri = "https://api.github.com/repos/$username/$Repo/git/trees/$commitSha";
+    $jsonResults = invoke-WebRequest -Method Get -Uri $Uri -Headers $headers;
+    $treeData = ConvertFrom-Json $jsonResults;
+
+    # todo: break if found
+    $folderSha = $null;
+    foreach ($item in $treeData.tree)
+    {
+        if (($item.path -eq $FolderPath) -and ($item.type -eq "tree"))
+        {
+            $folderSha = $item.sha;
+        }
+    }
+
+    # get sha of folder's tree
+    $Uri = "https://api.github.com/repos/$username/$Repo/git/trees/$folderSha";
+    $jsonResults = invoke-WebRequest -Method Get -Uri $Uri -Headers $headers;
+    $folderTreeData = ConvertFrom-Json $jsonResults;
+
+    # get all runbooks from the folder
+    # todo: open inner folders
+    foreach ($item in $folderTreeData.tree)
+    {
+        if (($item.type -eq "blob") -and ($item.path -match $psExtension))
+        {
+            # get name of the runbook
+            $pathSplit = $item.path.Split("/");
+            $filename = $pathSplit[$pathSplit.Count - 1];
+            $tempPathSplit = $filename.Split(".");
+            $runbookName = $tempPathSplit[0];
+
+            # get content of runbook
+            $itemSha = $item.sha;
+            $Uri = "https://api.github.com/repos/$username/$Repo/git/blobs/$itemSha";
+            $jsonResults = invoke-WebRequest -Method Get -Uri $Uri -Headers $headers;
+            $blobData = ConvertFrom-Json $jsonResults;
+            
+            InlineScript {
+                $content = $Using:blobData.content;
+                $bytes = [System.Convert]::FromBase64String($content);
+                $runbookDefinition = [System.Text.Encoding]::UTF8.GetString($bytes);
+                $Using:runbookName
+                $runbookDefinition  
+            }
+        }
+    }
 }
 
-Sync-GitHubRunbooks -Repo "MyRepo1" -OAuthToken "c601a5a48d5e3d88477be6ff1df343b78da2bd91" -Branch "tempBranch"
+Sync-GitHubRunbooks -Repo "MyRepo1" -OAuthToken "." -Branch "tempBranch"
+#empty repo error
+# Sync-GitHubRunbooks -Repo "TempRepo" -OAuthToken "." -Branch "tempBranch"
+# Sync-GitHubRunbooks -Repo "WrongNameRepo" -OAuthToken "." -Branch "tempBranch"
+
